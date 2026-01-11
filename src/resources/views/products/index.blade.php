e8269d659078a47e86e7a8286f3f1c17a1375acb
mariko inukai <mariko.inukai1006@gmail.com>
2026-01-11 22:30:09 +0900

    リモートリポジトリの変更

---
+++ src/resources/views/products/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>商品一覧</title>
+</head>
+
+<body>
+
+    <h1>商品一覧</h1>
+
+    <form method="GET">
+        <input
+            type="text"
+            name="keyword"
+            placeholder="商品名で検索">
+        <button type="submit">検索</button>
+    </form>
+
+    <ul>
+        <li>りんご / 300円</li>
+        <li>みかん / 200円</li>
+        <li>バナナ / 150円</li>
+    </ul>
+
+</body>
+
+</html>
